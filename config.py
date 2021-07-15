e5c1b4d8e51bdf4606afe73b07b49a567a3488cd
zhiyanov <zhiyanovap@gmail.com>
2021-07-15 13:31:12 +0300

    Changed config

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-FIRST_DATA_PATH = "./data/normal.csv"
-SECOND_DATA_PATH = "./data/tumored.csv"
-NETWORK_PATH = "./data/network.csv"
+DATA_PATH = "./data/data.csv"
+DESCRIPTION_PATH = "./data/description.csv"
+INTERACTION_PATH = "./data/interaction.csv"
 OUTPUT_DIR_PATH = "./data/"
 
 PROCESS_NUMBER = 4
