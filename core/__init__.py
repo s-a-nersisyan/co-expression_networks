8e3623edaa256c7ea3d2fc4ba3189916aed7b0e4
zhiyanov <zhiyanovap@gmail.com>
2021-07-19 20:31:45 +0300

    _proba --> _cdf, added paired_index python implementation

--- core/__init__.py
+++ core/__init__.py
@@ -5,14 +5,18 @@
 # Fast correlation computations
 from .fast_computations.correlations import \
     pearsonr, spearmanr
-
+from .fast_computations.tests import \
+    corr_diff_test
 
 # Probabilistic utlis
 from .correlation_utils import \
-    spearmanr_mean, spearmanr_std, spearmanr_proba, \
-    pearsonr_mean, pearsonr_std, pearsonr_proba, \
-    correlation_diff_proba, correlation_diff_analytic_proba
+    spearmanr_mean, spearmanr_std, spearmanr_cdf, \
+    pearsonr_mean, pearsonr_std, pearsonr_cdf, \
+    correlation_diff_cdf, correlation_diff_analytic_cdf
+
+from .utils import \
+    paired_index
 
 # Test utils
-from .correlation_tests import \
-    corr_diff_test
+# from .correlation_tests import \
+#     corr_diff_test
