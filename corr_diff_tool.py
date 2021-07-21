47b89e278b0e57789af7531dff54707b80557b8b
zhiyanov <zhiyanovap@gmail.com>
2021-07-21 07:48:10 +0300

    Exhaustive run

--- corr_diff_tool.py
+++ corr_diff_tool.py
@@ -14,6 +14,10 @@ from config import *
 data_df = pd.read_csv(DATA_PATH, sep=",", index_col=0)
 description_df = pd.read_csv(DESCRIPTION_PATH, sep=",")
 
+# Test mode
+# data_df = data_df.iloc[:100]
+# End of Test mode
+
 if (CORRELATION == "spearman"):
     correlation = core.spearmanr
 else:
@@ -61,7 +65,7 @@ exp_corrs = correlation(
     # numerical_index=numerical_flag
 )
 
-print("Test phase")
+print("Test phase", len(ref_corrs))
 stat, pvalue = core.corr_diff_test(
     ref_corrs.astype("float32"), np.zeros(len(ref_corrs), dtype="int32") +
         len(description_df.loc[description_df["Group"] == REFERENCE_GROUP]),
@@ -71,6 +75,19 @@ stat, pvalue = core.corr_diff_test(
     alternative=ALTERNATIVE,
     process_num=PROCESS_NUMBER
 )
+
+# Test mode
+# print("PyTest phase", len(ref_corrs))
+# py_stat, py_pvalue = core.py_corr_diff_test(
+#     ref_corrs.astype("float32"), np.zeros(len(ref_corrs), dtype="int32") +
+#         len(description_df.loc[description_df["Group"] == REFERENCE_GROUP]),
+#     exp_corrs.astype("float32"), np.zeros(len(exp_corrs), dtype="int32") +
+#         len(description_df.loc[description_df["Group"] == EXPERIMENTAL_GROUP]),
+#     correlation=CORRELATION,
+#     alternative=ALTERNATIVE,
+#     # process_num=PROCESS_NUMBER
+# )
+# End of test mode
 
 adjusted_pvalue = pvalue * len(pvalue) / \
     scipy.stats.rankdata(pvalue)
@@ -82,14 +99,15 @@ if INTERACTION_PATH:
     output_df = pd.DataFrame(interaction_df)
 
 else:
-    # indexes = np.where(adjusted_pvalue < FDR_THRESHOLD)
-    indexes = np.arange(100, dtype="int32")
+    indexes = np.where(adjusted_pvalue < FDR_THRESHOLD)
+    # indexes = np.arange(len(adjusted_pvalue), dtype="int32")
     ref_corrs = ref_corrs[indexes]
     exp_corrs = exp_corrs[indexes]
     stat = stat[indexes]
     pvalue = pvalue[indexes]
     adjusted_pvalue = adjusted_pvalue[indexes]
-    
+    # py_stat = py_stat[indexes]
+    # py_pvalue = py_pvalue[indexes]
     df_indexes = data_df.index.to_numpy()
     source_indexes = []
     target_indexes = []
@@ -106,5 +124,7 @@ output_df["Reference"] = ref_corrs
 output_df["Experimental"] = exp_corrs 
 output_df["Statistic"] = stat
 output_df["Pvalue"] = pvalue
+# output_df["PyStatistic"] = py_stat
+# output_df["PyPvalue"] = py_pvalue
 output_df["FDR"] = adjusted_pvalue
-output_df.to_csv(OUTPUT_DIR_PATH.rstrip("/") + "/{}_light_report.csv".format(CORRELATION), sep=",", index=None)
+output_df.to_csv(OUTPUT_DIR_PATH.rstrip("/") + "/{}_report.csv".format(CORRELATION), sep=",", index=None)
