463155f2166f21c5bae8afde3416e52fae30a17d
zhiyanov <zhiyanovap@gmail.com>
2021-09-07 17:09:15 +0300

    Bound correlations before test

--- core/fast_computations/tests.py
+++ core/fast_computations/tests.py
@@ -3,6 +3,18 @@ import numpy as np
 from .correlation_computations import _corr_diff_test
 from .correlation_computations import UNDEFINED_CORR_DIFF_TEST_VALUE
 
+LEFT_BORDER_BOUND = -1 + 1.e-6
+RIGHT_BORDER_BOUND = 1 - 1.e-6
+
+
+# This function is similar to the
+# one placed in utils.py
+def bound(array, left, right):
+    array = np.array(array)
+    array[array < left] = left
+    array[array > right] = right
+    return array
+
 def corr_diff_test(
     first_rs, first_size,
     second_rs, second_size,
@@ -10,9 +22,16 @@ def corr_diff_test(
     alternative="two-sided",
     process_num=1 
 ):
-    first_rs = np.array(first_rs, dtype="float32")
+    first_rs = np.array(
+        bound(first_rs, LEFT_BORDER_BOUND, RIGHT_BORDER_BOUND),
+        dtype="float32"
+    )
     first_size = np.array(first_size, dtype="int32")
-    second_rs = np.array(second_rs, dtype="float32")
+    
+    second_rs = np.array(
+        bound(second_rs, LEFT_BORDER_BOUND, RIGHT_BORDER_BOUND),
+        dtype="float32"
+    )
     second_size = np.array(second_size, dtype="int32")
     
     print("Test computations")
